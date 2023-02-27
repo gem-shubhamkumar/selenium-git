d6a13eb584a8f2f7577a1251a59ae3085bde1c01
gem-smitamishra <smita.mishra@geminisolutions.com>
2023-02-27 16:56:21 +0530

    latest change

--- src/test/java/org/example/locator/LoginLocator.java
+++ src/test/java/org/example/locator/LoginLocator.java
@@ -9,9 +9,8 @@ public class LoginLocator {
     public static By headerPage = By.xpath("//div[@class='oxd-topbar-header-title']//h6");
 
     public static By errorMessage(String field)
-
     {
-
       return   By.xpath("//label[text()='"+field+"']/../..//span");
     }
+
 }
